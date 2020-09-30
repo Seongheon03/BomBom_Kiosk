6d77632522d05d92ea3b216fd50712b9fbd329a9
Seongheon03 <56351656+Seongheon03@users.noreply.github.com>
2020-09-30 20:55:18 +0900

    [ADD] 메인 기능

--- BomBom_Kiosk/View/MainWindow.xaml.cs
+++ BomBom_Kiosk/View/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BomBom_Kiosk.View
 {
@@ -20,9 +11,62 @@ namespace BomBom_Kiosk.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        UserControl currentControl = new UserControl();
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            currentControl = ctrlHome;
+            SetBackground();
+            SetTimer();
+
+            DataContext = this;
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeControl(ctrlHome);
+        }
+
+        private void SetBackground()
+        {
+            var desktopWorkingArea = SystemParameters.WorkArea;
+
+            this.Top = desktopWorkingArea.Top;
+            this.Left = desktopWorkingArea.Left;
+            this.Height = desktopWorkingArea.Height;
+            this.Width = desktopWorkingArea.Width;
+        }
+
+        private void SetTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            SetTime();
+        }
+
+        private void SetTime()
+        {
+            tbCurrentTime.Text = DateTime.Now.ToString(string.Format("MM월 dd일 (ddd) tt hh시 mm분", CultureInfo.CreateSpecificCulture("ko-KR")));
+        }
+
+        private void ChangeControl(UserControl tragetControl)
+        {
+            currentControl.Visibility = Visibility.Collapsed;
+            currentControl = tragetControl;
+            currentControl.Visibility = Visibility.Visible;
         }
     }
 }
