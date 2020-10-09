412ccde192cf762a69b41887617c57aae4acb5fd
Mary Oh <maryoh2002@gmail.com>
2020-10-09 22:23:29 +0900

    [ADD] 테이블 선택 화면 로직

---
+++ BomBom_Kiosk/Model/Table.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomBom_Kiosk.Model
+{
+    public class Table
+    {
+        public int Number { get; set; }
+        public string LeftTime { get; set; }
+    }
+}
