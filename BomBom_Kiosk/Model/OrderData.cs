bb680d7520d7b4754e67de6ceb292d9da4110830
Mary Oh <maryoh2002@gmail.com>
2020-10-14 14:52:44 +0900

    [ADD] orderData model

---
+++ BomBom_Kiosk/Model/OrderData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomBom_Kiosk.Model
+{
+    public class OrderData
+    {
+        public EOrderPlace Place { get; set; }
+
+        public EOrderType Type { get; set; }
+
+        public int Table { get; set; }
+        
+    }
+
+    public enum EOrderPlace
+    {
+        InShop,
+        Packing
+    }
+
+    public enum EOrderType
+    {
+        Cash,
+        Card
+    }
+}
