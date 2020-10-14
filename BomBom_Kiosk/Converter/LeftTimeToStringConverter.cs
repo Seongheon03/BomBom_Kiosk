7137c7b1e99a4f88d512b0031f131551b142e1c7
Mary Oh <maryoh2002@gmail.com>
2020-10-14 14:52:18 +0900

    [ADD] TimeConverter

---
+++ BomBom_Kiosk/Converter/LeftTimeToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BomBom_Kiosk.Converter
+{
+    public class LeftTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string time = value.ToString();
+
+            return time;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
