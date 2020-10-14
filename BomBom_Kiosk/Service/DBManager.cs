1e1956e52aee3f73ed6a52b2280d5f0a988be906
Seongheon03 <56351656+Seongheon03@users.noreply.github.com>
2020-10-14 13:45:45 +0900

    [ADD] DBManager, UIManager

---
+++ BomBom_Kiosk/Service/DBManager.cs
@@ -0,0 +1,47 @@
+﻿using BomBom_Kiosk.Model;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace BomBom_Kiosk.Service
+{
+    public class DBManager
+    {
+        public List<Drink> GetDrinks()
+        {
+            List<Drink> drinks = new List<Drink>();
+
+            MySqlConnection conn = new MySqlConnection(App.connStr);
+            MySqlCommand cmd = conn.CreateCommand();
+
+            string query = "SELECT * FROM menu";
+            cmd.CommandText = query;
+
+            try
+            {
+                conn.Open();
+            }
+            catch
+            {
+                MessageBox.Show("DB 연결이 되지 않았습니다.");
+                return drinks;
+            }
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Drink drink = new Drink();
+                drink.Idx = int.Parse(reader["idx"].ToString());
+                drink.Name = reader["name"].ToString();
+                drink.Price = int.Parse(reader["price"].ToString());
+                drink.Image = reader["image"].ToString();
+                drink.Category = (ECategory)int.Parse(reader["type"].ToString());
+
+                drinks.Add(drink);
+            }
+
+            return drinks;
+        }
+    }
+}
---
+++ BomBom_Kiosk/Service/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BomBom_Kiosk.Service
+{
+    public class UIManager
+    {
+        public Stack<UserControl> uiStack = new Stack<UserControl>();
+
+        public void Push(UserControl userControl)
+        {
+            if (uiStack.Count != 0)
+            {
+                uiStack.Peek().Visibility = Visibility.Collapsed;
+            }
+
+            uiStack.Push(userControl);
+            SetVisible(userControl, Visibility.Visible);
+        }
+
+        public void Pop()
+        {
+            uiStack.Pop();
+
+            if (uiStack.Count == 0)
+            {
+                return;
+            }
+
+            SetVisible(uiStack.Peek(), Visibility.Collapsed);
+        }
+
+        public void SetVisible(UserControl userControl, Visibility visible)
+        {
+            userControl.Visibility = visible;
+        }
+    }
+}
