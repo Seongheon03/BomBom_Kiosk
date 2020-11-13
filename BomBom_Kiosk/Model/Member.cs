2b6dc9a682c9831fd0ae6dd63f30f18191dce428
Mary Oh <maryoh2002@gmail.com>
2020-11-13 11:26:50 +0900

    [ADD] networkManager

---
+++ BomBom_Kiosk/Model/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomBom_Kiosk.Model
+{
+    public class Member
+    {
+        public string Id { get; set; }
+        public string Pw { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+    }
+}
