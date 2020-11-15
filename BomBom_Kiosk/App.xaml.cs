9d4d0d4b0ce9caf9bf75a239774c2d735ffe8adb
Seongheon03 <56351656+Seongheon03@users.noreply.github.com>
2020-11-15 16:00:37 +0900

    [UPDATE] 로그인 처리

--- BomBom_Kiosk/App.xaml.cs
+++ BomBom_Kiosk/App.xaml.cs
@@ -16,25 +16,5 @@ namespace BomBom_Kiosk
         public static UIManager uiManager = new UIManager();
         public static OrderViewModel orderViewModel = new OrderViewModel();
         public static PaymentViewModel paymentViewModel = new PaymentViewModel();
-
-        public delegate void LoadingEventHandler(bool isLoading, string status);
-        public static event LoadingEventHandler LoadingAction;
-
-        public static async Task InitData()
-        {
-            LoadingAction?.Invoke(true, "DB에 연결중입니다...");
-            if (await Task.Run(() => dbManager.ConnectDB()))
-            {
-                LoadingAction?.Invoke(true, "메뉴를 불러오는 중입니다...");
-                await Task.Run(() => orderViewModel.InitData());
-                LoadingAction?.Invoke(true, "회원을 불러오는 중입니다...");
-                await Task.Run(() => paymentViewModel.InitMembers());
-                LoadingAction?.Invoke(false, "로그인을 해주세요.");
-            }
-            else
-            {
-                LoadingAction?.Invoke(false, "DB에 연결되지 않았습니다.");
-            }
-        }
     }
 }
