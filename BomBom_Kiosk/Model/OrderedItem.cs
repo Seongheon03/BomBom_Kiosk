ad14b7b957e61a9fabdd1912e49da64349626871
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-27 10:29:50 +0900

    [ADD]

---
+++ BomBom_Kiosk/Model/OrderedItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomBom_Kiosk.Model
+{
+    public class OrderedItem
+    {
+        public int Idx { get; set; }
+        public MenuModel Menu { get; set; }
+        public MemberModel Member { get; set; }
+        public int Count { get; set; }
+        public int Seat { get; set; }
+        public string OrderCode { get; set; }
+        public EOrderPlace Place { get; set; }
+        public EOrderType Type { get; set; }
+        public DateTime OrderDateTime { get; set; }
+    }
+    public class MenuModel
+    {
+        public int Idx { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Price { get; set; }
+        public int Type { get; set; }
+        public int DiscountPrice { get; set; }
+    }
+    public class MemberModel
+    {
+        public int Idx { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Id { get; set; }
+        public string Pw { get; set; }
+    }
+}
